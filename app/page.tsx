b5590782328af9317fabe7f2934f4fd0619a62d1
Sonya Khosravani <i562410@fontysict.nl>
2026-06-09 10:54:27 +0200

    ik heb uit de eerste twee branches ( Overzicht & filter) gepulled. Ook heb ik de code weer erin gezet die ik er eerder uithaalde om een conflict te voorkomen. Ze zijn nu beide zichtbaar in de browser

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import CategorieFilters from "../componenten/filter";
+import Overzicht from "../componenten/overzicht";
 
 export default function Page() {
   return (
     <main>
+      <Overzicht datum="Donderdag — 4 Juni 2026" voltooid={2} totaal={7} />
       <CategorieFilters />
     </main>
   );
